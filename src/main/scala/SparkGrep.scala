d4a6ac52f03eaca23ef964053d559e5addfa82cf
andrewjbaumann <baumann.andrew14@gmail.com>
2015-05-10 17:44:56 -0400

    spark is amazing

--- src/main/scala/SparkGrep.scala
+++ src/main/scala/SparkGrep.scala
@@ -35,60 +35,63 @@ object SparkGrep {
     val sc = new SparkContext(conf)
     val inputFile = sc.textFile(args(1), 2).cache()
     val counts = inputFile.flatMap(line => line.split(" "))
-      .map(word => word)
+      .filter(word => word.contains("gene_"))
+      .distinct()
+      
+
     println("Spark code done")
 
-    val tempCounts = counts.toArray()
-    val tempList:Set[String] = tempCounts.toSet
-    var aList:Set[String] = Set()
+    counts.saveAsTextFile("bin/output")
 
-    println("Starting iterate...")
+    def distance(s1: String, s2: String): Int = {
+      def minimum(i1: Int, i2: Int, i3: Int) = min(min(i1, i2), i3)
+      val dist = Array.tabulate(s2.length + 1, s1.length + 1) { (j, i) => if (j == 0) i else if (i == 0) j else 0 }
 
-    iterate(tempList)
-    def iterate(theCounters:Set[String]):Unit =
-    {
-      if (theCounters.size == 0)
-        return
-      if (theCounters.head.startsWith("gene_"))
-      {
-        aList = aList + theCounters.head
-      }
-      iterate(theCounters.tail)
+      for (j <- 1 to s2.length; i <- 1 to s1.length)
+        dist(j)(i) = if (s2(j - 1) == s1(i - 1)) dist(j - 1)(i - 1)
+        else minimum(dist(j - 1)(i) + 1, dist(j)(i - 1) + 1, dist(j - 1)(i - 1) + 1)
+      dist(s2.length)(s1.length)
     }
-
-    val aRay:Array[String] = aList.toArray
-    var secondRay:Array[String] = aList.toArray
-    var finalArray = Array[Tuple2[String,String]]()
-    consolidate(aRay)
-
-    def consolidate(array:Array[String]):Unit =
-    {
-      for(x <- 0 until (array.length-1))
-      {
-        var temp = array(x+1)
-
-        for(y <- x+1 until (array.length-1))
-          if(distance(array(x),temp) < distance(array(x), array(y)))
-            temp = array(y)
-
-        val t = (array(x),temp)
-        finalArray = finalArray :+ t
-      }
-    }
-    finalArray.foreach(x => println(x))
-    //counts.saveAsTextFile("bin/output")
-
-    def distance(s1:String, s2:String):Int =
-    {
-      def minimum(i1: Int, i2: Int, i3: Int)=min(min(i1, i2), i3)
-      val dist=Array.tabulate(s2.length+1, s1.length+1){(j,i)=>if(j==0) i else if (i==0) j else 0}
-
-      for(j<-1 to s2.length; i<-1 to s1.length)
-        dist(j)(i)=if(s2(j-1)==s1(i-1)) dist(j-1)(i-1)
-        else minimum(dist(j-1)(i)+1, dist(j)(i-1)+1, dist(j-1)(i-1)+1)
-        dist(s2.length)(s1.length)
-    }
-
     System.exit(0)
   }
 }
+
+
+//Previous Code
+/*
+def Looking_for_genes(s: String): Unit = {
+  val tempCounts = counts.toArray()
+  val tempList: Set[String] = tempCounts.toSet
+  var aList: Set[String] = Set()
+
+  println("Starting iterate...")
+
+  iterate(tempList)
+  def iterate(theCounters: Set[String]): Unit = {
+    if (theCounters.size == 0)
+      return
+    if (theCounters.head.startsWith("gene_")) {
+      aList = aList + theCounters.head
+    }
+    iterate(theCounters.tail)
+  }
+
+  val aRay: Array[String] = aList.toArray
+  var secondRay: Array[String] = aList.toArray
+  var finalArray = Array[Tuple2[String, String]]()
+  consolidate(aRay)
+
+  def consolidate(array: Array[String]): Unit = {
+    for (x <- 0 until (array.length - 1)) {
+      var temp = array(x + 1)
+
+      for (y <- x + 1 until (array.length - 1))
+        if (distance(array(x), temp) < distance(array(x), array(y)))
+          temp = array(y)
+
+      val t = (array(x), temp)
+      finalArray = finalArray :+ t
+    }
+  }
+  finalArray.foreach(x => println(x))
+}*/
