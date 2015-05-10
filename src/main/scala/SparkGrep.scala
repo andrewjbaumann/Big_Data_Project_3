88947456973ce67fe8464bdd101fb62002f38b20
andrewjbaumann <baumann.andrew14@gmail.com>
2015-05-09 20:23:33 -0400

    working, more robust test

--- src/main/scala/SparkGrep.scala
+++ src/main/scala/SparkGrep.scala
@@ -17,8 +17,10 @@
  *              To compile: scalac *.scala
  *              To run:     scala SparkGrep <host> <input_file>
  */
-
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.math._
+
 
 object SparkGrep {
   def main(args: Array[String]) {
@@ -26,27 +28,66 @@ object SparkGrep {
       System.err.println("Usage: SparkGrep <host> <input_file>")
       System.exit(1)
     }
+    println("Starting")
 
 
     val conf = new SparkConf().setAppName("SparkGrep").setMaster(args(0))
     val sc = new SparkContext(conf)
     val inputFile = sc.textFile(args(1), 2).cache()
     val counts = inputFile.flatMap(line => line.split(" "))
-      .map(word => (word))
-    //counts.saveAsTextFile("bin/output")
-    val arrayCounts = counts.toArray()
-    var finalArray:List[String] = List()
-    iterate(arrayCounts)
+      .map(word => word)
+    println("Spark code done")
 
-    def iterate(theArray:Array[String]): Unit ={
-      for (x <- theArray) {
-        if (theArray.head.toString().startsWith("gene")) {
-          finalArray = finalArray.::(arrayCounts.head.toString())
-        }
+    val tempCounts = counts.toArray()
+    val tempList:Set[String] = tempCounts.toSet
+    var aList:Set[String] = Set()
+
+    println("Starting iterate...")
+
+    iterate(tempList)
+    def iterate(theCounters:Set[String]):Unit =
+    {
+      if (theCounters.size == 0)
+        return
+      if (theCounters.head.startsWith("gene_"))
+      {
+        aList = aList + theCounters.head
       }
-      iterate(theArray.tail)
+      iterate(theCounters.tail)
     }
 
+    val aRay:Array[String] = aList.toArray
+    var secondRay:Array[String] = aList.toArray
+    var finalArray = Array[Tuple2[String,String]]()
+    consolidate(aRay)
+
+    def consolidate(array:Array[String]):Unit =
+    {
+      for(x <- 0 until (array.length-1))
+      {
+        var temp = array(x+1)
+
+        for(y <- x+1 until (array.length-1))
+          if(distance(array(x),temp) < distance(array(x), array(y)))
+            temp = array(y)
+
+        val t = (array(x),temp)
+        finalArray = finalArray :+ t
+      }
+    }
+    finalArray.foreach(x => println(x))
+    //counts.saveAsTextFile("bin/output")
+
+    def distance(s1:String, s2:String):Int =
+    {
+      def minimum(i1: Int, i2: Int, i3: Int)=min(min(i1, i2), i3)
+      val dist=Array.tabulate(s2.length+1, s1.length+1){(j,i)=>if(j==0) i else if (i==0) j else 0}
+
+      for(j<-1 to s2.length; i<-1 to s1.length)
+        dist(j)(i)=if(s2(j-1)==s1(i-1)) dist(j-1)(i-1)
+        else minimum(dist(j-1)(i)+1, dist(j)(i-1)+1, dist(j-1)(i-1)+1)
+        dist(s2.length)(s1.length)
+    }
 
     System.exit(0)
   }
