c8218a0dac789fe36b063384f1b7bad198145f6c
andrewjbaumann <baumann.andrew14@gmail.com>
2015-05-10 23:12:38 -0400

    working with spark continues

--- build.sbt
+++ build.sbt
@@ -8,3 +8,4 @@ fork := true
 
 libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "1.3.1")
 
+libraryDependencies += "org.scala-lang" % "scala-actors" % "2.11.6"
--- src/main/scala/SparkGrep.scala
+++ src/main/scala/SparkGrep.scala
@@ -16,33 +16,23 @@
  * Build with:  Scala IDE (Eclipse or IntelliJ) or using the following commands on the glab machines
  *              To compile: scalac *.scala
  *              To run:     scala SparkGrep <host> <input_file>
+ * Useful Websites:
+ * https://spark.apache.org/docs/latest/programming-guide.html
+ * https://spark.apache.org/docs/latest/configuration.html
+ * https://spark.apache.org/docs/latest/programming-guide.html
  */
+
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.math._
 
-
 object SparkGrep {
-  def main(args: Array[String]) {
-    if (args.length < 2) {
-      System.err.println("Usage: SparkGrep <host> <input_file>")
-      System.exit(1)
-    }
-    println("Starting")
+  //dist is supposed to take 2 genes a return the edit distance
+  //but i haven't decided how. whether to pass the rdd through
+  //the function itself, or maybe just two strings, or even
+  //possibly an array or something like that
 
-
-    val conf = new SparkConf().setAppName("SparkGrep").setMaster(args(0))
-    val sc = new SparkContext(conf)
-    val inputFile = sc.textFile(args(1), 2).cache()
-    val counts = inputFile.flatMap(line => line.split(" "))
-      .filter(word => word.contains("gene_"))
-      .distinct()
-      
-
-    println("Spark code done")
-
-    counts.saveAsTextFile("bin/output")
-
+  def dist(a:Array[String], c:String): String = {
     def distance(s1: String, s2: String): Int = {
       def minimum(i1: Int, i2: Int, i3: Int) = min(min(i1, i2), i3)
       val dist = Array.tabulate(s2.length + 1, s1.length + 1) { (j, i) => if (j == 0) i else if (i == 0) j else 0 }
@@ -52,6 +42,52 @@ object SparkGrep {
         else minimum(dist(j - 1)(i) + 1, dist(j)(i - 1) + 1, dist(j - 1)(i - 1) + 1)
       dist(s2.length)(s1.length)
     }
+    var min_string = ""
+    for(x <- 0 to a.size-1)
+    {
+      var max_dist = 0
+
+      var compute = distance(c, a(x))
+
+      if(compute > max_dist)
+      {
+        max_dist = compute
+        min_string = a(x)
+      }
+    }
+    println(min_string)
+    min_string
+  }
+
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: SparkGrep <host> <input_file>")
+      System.exit(1)
+    }
+    println("Starting")
+
+    val conf = new SparkConf().setAppName("SparkGrep").setMaster(args(0))
+    val sc = new SparkContext(conf)
+    val inputFile = sc.textFile(args(1), 2).cache()
+
+    val counts = inputFile.flatMap(line => line.split(" "))
+      .filter(word => word.contains("gene_"))
+      .distinct()
+    /*
+    This code snippet is supposed to take the whole list of genes and compare them to every other gene in the list,
+    but it's way too large and takes way too long. Spark doesn't let you call another RDD inside of an RDD
+    (for example, letting a new var RDD map the words into tuples of (word, distance(counts, word)) won't work
+    because you can't call an RDD inside of another. So I don't think at this point there's a really elegant way
+    for spark to handle something of this kind... but at the same time, that sounds kind of dumb, but i'll figure it
+    out tuesday.
+    val array = counts.toArray()
+    val results = counts
+      .map(word => (word, dist(array, word)))
+     */
+
+    println("Spark code done")
+
+    counts.saveAsTextFile("bin/output")
     System.exit(0)
   }
 }
